d68857f309a6a2571f5f47d259558ed90f123596
Julio Conchas <jconchas@ibm.com>
2023-11-29 11:19:20 -0600

    adding I Am Poor project

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Julio Conchas on 28/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I Am Poor/README.md
@@ -0,0 +1,3 @@
+# I Am Poor
+
+Julio Conchas
