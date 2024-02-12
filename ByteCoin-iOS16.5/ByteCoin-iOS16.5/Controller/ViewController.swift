a0b07023817ad29c568a57c97325b44be493a0af
Julio Conchas <jconchas@ibm.com>
2024-02-12 11:56:40 -0600

    ByteCoin for iOS16.5 starting project

---
+++ ByteCoin-iOS16.5/ByteCoin-iOS16.5/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin-iOS16.5
+//
+//  Created by Julio Conchas on 12/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
