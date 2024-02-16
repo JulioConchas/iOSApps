1a9e2dfcdbca42d43cb51173ff8c7154c3c04258
Julio Conchas <jconchas@ibm.com>
2024-02-16 07:08:23 -0600

    loogin thru title label to create an animation when init app

--- TaanChat-iOS16.5/TaanChat-iOS16.5/Controller/ViewController.swift
+++ TaanChat-iOS16.5/TaanChat-iOS16.5/Controller/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Animation for titile
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "📣 T'aan"
+        for letter in titleText
+        {
+            // timer Closure
+            Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
 
 
