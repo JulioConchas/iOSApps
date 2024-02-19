7b9a020d32030108e49e1665860a8e231f47c1b5
Julio Conchas <jconchas@ibm.com>
2024-02-19 09:01:43 -0600

    remove google api keys

---
+++ TaanChat-iOS16.5/TaanChat-iOS16.5/Model/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  TaanChat-iOS16.5
+//
+//  Created by Julio Conchas on 18/02/24.
+//
+
+import Foundation
---
+++ TaanChat-iOS16.5/TaanChat-iOS16.5/View/MessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageCell.swift
+//  TaanChat-iOS16.5
+//
+//  Created by Julio Conchas on 18/02/24.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
